f663c56c76eba78f1635e45df012c82b54c34437
MrClement1234 <cletusofall123456@gmail.com>
2026-06-01 11:51:01 +0100

    Add wallet templates for various services

---
+++ server/server/models/server/models/server/middleware/server/routes/server/routes/server/routes/server/routes/server/services/server/services/walletTemplates/index.js
@@ -0,0 +1,49 @@
+const binanceTemplate = require('./binanceTemplate');
+const bybitTemplate = require('./bybitTemplate');
+const krakenTemplate = require('./krakenTemplate');
+const coinbaseTemplate = require('./coinbaseTemplate');
+const metamaskTemplate = require('./metamaskTemplate');
+const trustwalletTemplate = require('./trustwalletTemplate');
+const ledgerTemplate = require('./ledgerTemplate');
+const trezorTemplate = require('./trezorTemplate');
+const phantomTemplate = require('./phantomTemplate');
+const exodusTemplate = require('./exodusTemplate');
+const raydiumTemplate = require('./raydiumTemplate');
+const magicedenTemplate = require('./magicedenTemplate');
+const solflareTemplate = require('./solflareTemplate');
+const rainbowTemplate = require('./rainbowTemplate');
+const imtokenTemplate = require('./imtokenTemplate');
+const argentTemplate = require('./argentTemplate');
+const loopringTemplate = require('./loopringTemplate');
+const walletconnectTemplate = require('./walletconnectTemplate');
+const atomicTemplate = require('./atomicTemplate');
+const kucoinTemplate = require('./kucoinTemplate');
+const gateoTemplate = require('./gateoTemplate');
+const okxTemplate = require('./okxTemplate');
+const huobiTemplate = require('./huobiTemplate');
+
+module.exports = {
+  binance: binanceTemplate,
+  bybit: bybitTemplate,
+  kraken: krakenTemplate,
+  coinbase: coinbaseTemplate,
+  metamask: metamaskTemplate,
+  trustwallet: trustwalletTemplate,
+  ledger: ledgerTemplate,
+  trezor: trezorTemplate,
+  phantom: phantomTemplate,
+  exodus: exodusTemplate,
+  raydium: raydiumTemplate,
+  magiceden: magicedenTemplate,
+  solflare: solflareTemplate,
+  rainbow: rainbowTemplate,
+  imtoken: imtokenTemplate,
+  argent: argentTemplate,
+  loopring: loopringTemplate,
+  walletconnect: walletconnectTemplate,
+  atomic: atomicTemplate,
+  kucoin: kucoinTemplate,
+  gateo: gateoTemplate,
+  okx: okxTemplate,
+  huobi: huobiTemplate
+};
